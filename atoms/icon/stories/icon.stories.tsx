e092abe68d7555c030d6cfa644a8337d516b1808
Gregor Adams <gregor.adams@evernest.com>
2020-09-17 15:10:23 +0200

    docs(icon): adjust stories

--- atoms/icon/stories/icon.stories.tsx
+++ atoms/icon/stories/icon.stories.tsx
@@ -1,60 +1,24 @@
-import { Wrapper } from "@evernest/dev-helpers";
-import { icons } from "@evernest/icons";
-import { select } from "@storybook/addon-knobs";
 import { withTests } from "@storybook/addon-jest";
 import React from "react";
-import humanize from "humanize-string";
-import { Icon, IconName, Size } from "../src";
+import { Icon, IconProps } from "../src";
 import results from "./icon.testresults.json";
+import { Story } from "@storybook/react";
+import { withWrapper } from "@evernest/dev-helpers";
 
-const iconOptions = Object.keys(icons["24"]).reduce((previousValue, currentValue) => {
-	return { ...previousValue, [humanize(currentValue)]: currentValue };
-}, {});
-
-const sizeOptions = Object.entries(Size)
-	.filter(([, value]) => typeof value === "number")
-	.reduce((previousValue, [key, value]) => {
-		return { ...previousValue, [humanize(key)]: value };
-	}, {});
-
-export const simple: React.FC = () => {
-	const icon = select("Icon Name", iconOptions, "accessibility") as IconName;
-	const size = select("Icon Size", sizeOptions, Size.medium) as Size;
-	return (
-		<Wrapper>
-			<Icon icon={icon} size={size} />
-		</Wrapper>
-	);
+const Template: Story<IconProps> = args => {
+	return <Icon {...args} />;
 };
 
-export const allSizes: React.FC = () => {
-	const icon = select("Icon Name", iconOptions, "accessibility") as IconName;
-	return (
-		<Wrapper>
-			{Object.entries(Size)
-				.filter(([, value]) => typeof value === "number")
-				.map(([size]) => {
-					return <Icon key={icon} icon={icon} size={Size[size]} />;
-				})}
-		</Wrapper>
-	);
-};
+export const Simple = Template.bind({});
 
-export const allIcons: React.FC = () => {
-	const size = select("Icon Size", sizeOptions, Size.medium) as Size;
-	return (
-		<Wrapper>
-			{Object.keys(icons[size]).map((icon: IconName) => (
-				<Icon key={icon} icon={icon} size={size} />
-			))}
-		</Wrapper>
-	);
+Simple.args = {
+	icon: "instagram",
 };
 
 const story = {
 	component: Icon,
 	title: "Design System/Atoms/Icon",
-	decorators: [withTests({ results })],
+	decorators: [withTests({ results }), withWrapper()],
 	parameters: {
 		jest: ["icon"],
 	},
