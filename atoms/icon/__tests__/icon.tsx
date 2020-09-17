7c2d7616121ef8d4f11c9f72f9dbd0007c56ad9f
Gregor Adams <gregor.adams@evernest.com>
2020-09-17 15:08:38 +0200

    test(icon): adjust test

--- atoms/icon/__tests__/icon.tsx
+++ atoms/icon/__tests__/icon.tsx
@@ -5,7 +5,7 @@ import { Icon } from "../src";
 test("Icon contains text", () => {
 	const wrapper = mount(
 		<div>
-			<Icon icon="accessibility" />
+			<Icon icon="instagram" />
 		</div>
 	);
 	expect(wrapper.find(Icon).text()).toEqual("Accessibility");
@@ -15,7 +15,7 @@ test("Icon allows setting classname", () => {
 	const className = "test";
 	const wrapper = mount(
 		<div>
-			<Icon className={className} icon="accessibility" />
+			<Icon className={className} icon="instagram" />
 		</div>
 	);
 	expect(wrapper.find(Icon)).toHaveClassName(className);
