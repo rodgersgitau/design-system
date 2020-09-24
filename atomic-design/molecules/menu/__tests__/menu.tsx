f039393ff3260decb55e22fbde93a9be856e77a5
Gregor Adams <pixelass@users.noreply.github.com>
2020-09-24 11:28:29 +0200

    Test/menu (#10)
    * chore(menu): add test scripts
    
    * test(menu): add missing tests

--- atomic-design/molecules/menu/__tests__/menu.tsx
+++ atomic-design/molecules/menu/__tests__/menu.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { mount } from "enzyme";
-import { Menu } from "../src";
-import { Wrapper } from "@evernest/dev-helpers";
+import { Menu, MenuDirection, StyledMenuProps } from "../src";
+import { createTree, withTheme } from "@evernest/dev-helpers";
+import { theme } from "@evernest/theme";
+
+const ThemedMenu = withTheme<StyledMenuProps>(Menu, theme);
 
 test("Menu only renders children", () => {
 	const text = "Hello Grid";
-	const wrapper = mount(
-		<Wrapper>
-			<Menu>{text}</Menu>
-		</Wrapper>
-	);
-	expect(wrapper.find(Menu).text()).toEqual(text);
+	const wrapper = mount(<ThemedMenu>{text}</ThemedMenu>);
+	expect(wrapper.text()).toEqual(text);
 });
 
 test("Menu allows setting classname", () => {
 	const className = "test";
-	const wrapper = mount(
-		<Wrapper>
-			<Menu className={className} />
-		</Wrapper>
-	);
-	expect(wrapper.find(Menu)).toHaveClassName(className);
+	const wrapper = mount(<ThemedMenu className={className} />);
+	expect(wrapper).toHaveClassName(className);
+});
+
+test("Menu can be vertical or horizontal", () => {
+	const vertical = createTree(<ThemedMenu direction={MenuDirection.vertical} />);
+	const horizontal = createTree(<ThemedMenu direction={MenuDirection.horizontal} />);
+	expect(vertical).toMatchSnapshot();
+	expect(horizontal).toMatchSnapshot();
 });
